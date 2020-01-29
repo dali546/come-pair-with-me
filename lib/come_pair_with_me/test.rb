b4ceab7aae5847ab93ae75da5f70d68f9620b2cc
muhammed-ali <muhammed.ali@onthebeach.co.uk>
2020-01-29 15:33:58 +0000

    block kit test

--- lib/come_pair_with_me/test.rb
+++ lib/come_pair_with_me/test.rb
@@ -1,7 +1,13 @@
 module ComePairWithMe
   class Bot < SlackRubyBot::Bot
     command /test/ do |client, data, match|
-      client.say(channel: data.channel, text: 'Test Command')
+      client.say(channel: data.channel, text: '{
+        "type": "section",
+        "text": {
+          "type": "mrkdwn",
+          "text": "A message *with some bold text* and _some italicized text_."
+        }
+      }')
     end
   end
 end
