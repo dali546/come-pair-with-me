54aafa518150c4ac34c20052bbb46c308408f4a5
muhammed-ali <muhammed.ali@onthebeach.co.uk>
2020-01-29 15:37:59 +0000

    Added Button Response

--- lib/bot.rb
+++ lib/bot.rb
@@ -1,7 +1,33 @@
 module ComePairWithMe
   class Bot < SlackRubyBot::Bot
     command /pair/ do |client, data, match|
-      client.say(text: 'I will Pair With You', channel: data.channel)
+      client.say(text: '{"blocks": [
+            {
+                "type": "section",
+                "text": {
+                    "type": "mrkdwn",
+                    "text": "A person has requested to pair for such and such a reason"
+                },
+                "block_id": "text1"
+            },
+            {
+                "type": "section",
+                "text": {
+                    "type": "mrkdwn",
+                    "text": "Click to accept their Pairing Request"
+                },
+                "accessory": {
+                    "type": "button",
+                    "text": {
+                        "type": "plain_text",
+                        "text": "I\'ll Help",
+                        "emoji": true
+                    },
+                    "value": "click_me_123"
+                }
+            }
+        ]
+    }', channel: data.channel)
     end
   end
 end
